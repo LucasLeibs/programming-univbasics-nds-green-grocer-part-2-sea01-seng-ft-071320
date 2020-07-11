90b717bc249efdf9eac1fccb40e8ba89353a5fe8
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-07-11 19:47:19 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,28 +1,29 @@
 require_relative './part_1_solution.rb'
 
-def apply_coupons(cart, coupons)
- coupons.each do |coupon|
-   coupon.each do |attributes, value|
-     name = coupon[:item]
- 
- if cart[name] && cart[name][:count]
-   
-   if cart["#{name} W/COUPON"]
-     cart["#{name} W/COUPON"][:count] += 1 
-   else 
-     cart["#{name} W?COUPON"] = {:price => coupon[:cost], :clearance => cart[name][:clearance], :count => 1}
-   end
-      cart[name[:count] -= coupon[:num]
+
+ def apply_coupons(cart, coupons) 
+   coupons.each do |coupon| 
+    coupon.each do |attribute, value| 
+      name = coupon[:item] 
+    
+      if cart[name] && cart[name][:count] >= coupon[:num] 
+        if cart["#{name} W/COUPON"] 
+          cart["#{name} W/COUPON"][:count] += 1 
+        else 
+          cart["#{name} W/COUPON"] = {:price => coupon[:cost], 
+          :clearance => cart[name][:clearance], :count => 1} 
+        end 
+  
+      cart[name][:count] -= coupon[:num] 
     end 
-  end
+  end 
+end 
+  cart 
 end
-  cart
-end
+
+
  
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
-end
+ 
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
